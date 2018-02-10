b243f4ab2125c216d28503ffe62a32acb23db06b
mrtehnuke <davidsgabalins@gmail.com>
2018-02-10 17:09:08 +0200

    edited navbar, installed font-awesome

--- src/includes/partials/navbar.php
+++ src/includes/partials/navbar.php
@@ -1,21 +1,23 @@
 <nav class="navbar navbar-light navbar-expand-md fixed-top">
-    <a class="navbar-brand" href="/">Name/logo</a>
-    <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" 
-            data-target="#navbarCollapse" aria-controls="navbarCollapse" 
-            aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarCollapse">
-        <ul class="navbar-nav ml-auto text-right">
-            <li class="nav-item">
-                <a class="nav-link" href="/">HOME</a><hr>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/about">ABOUT US</a><hr>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/contact">CONTACT US</a>
-            </li>
-        </ul>
-    </div>
+    <div class="container">
+        <a class="navbar-brand" href="/">Name/logo</a>
+        <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" 
+                data-target="#navbarCollapse" aria-controls="navbarCollapse" 
+                aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav ml-auto text-right">
+                <li class="nav-item">
+                    <a class="nav-link" href="/">HOME</a><hr>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/about">ABOUT US</a><hr>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/contact">CONTACT US</a>
+                </li>
+            </ul>
+        </div>
+    </div>    
 </nav>
